87ec9c11bc18d2ce1ec97a3fb8e001d7215b6292
Riley Campbell <rileydcampbell@berkeley.edu>
2023-09-14 11:50:57 -0400

    Add approve and permit flow.

--- src/FeeCollector.sol
+++ src/FeeCollector.sol
@@ -4,37 +4,35 @@ pragma solidity ^0.8.19;
 import {Owned} from "solmate/auth/Owned.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
-import {IUniversalRouter} from "./external/IUniversalRouter.sol";
 import {IFeeCollector} from "./interfaces/IFeeCollector.sol";
 
 /// @notice The collector of protocol fees that will be used to swap and send to a fee recipient address.
 contract FeeCollector is Owned, IFeeCollector {
     using SafeTransferLib for ERC20;
 
-    IUniversalRouter private immutable universalRouter;
+    error InvalidUniversalRouterCalldata();
+
+    address private universalRouter;
     address private immutable feeRecipient;
     ERC20 private immutable feeToken;
 
     constructor(address _owner, address _universalRouter, address _feeRecipient, address _feeToken) Owned(_owner) {
-        universalRouter = IUniversalRouter(_universalRouter);
+        universalRouter = _universalRouter;
         feeRecipient = _feeRecipient;
         feeToken = ERC20(_feeToken);
     }
 
-    /// @inheritdoc IFeeCollector
-    function swapBalance(address[] calldata tokens, bytes calldata commands, bytes[] calldata inputs, uint48 deadline)
-        external
-        onlyOwner
-    {
-        for (uint256 i = 0; i < tokens.length; i++) {
-            ERC20 token = ERC20(tokens[i]);
-            uint256 balance = token.balanceOf(address(this));
-            if (balance > 0) {
-                token.safeTransfer(address(universalRouter), balance);
-            }
+    function approveAndPermit(ERC20[] calldata tokensToApprove) external onlyOwner {
+        for (uint256 i = 0; i < tokensToApprove.length; i++) {
+            tokensToApprove[i].approve(address(universalRouter), type(uint256).max);
+            tokensToApprove[i].permit(address(this), address(universalRouter), type(uint256).max, type(uint256).max);
         }
+    }
 
-        universalRouter.execute(commands, inputs, deadline);
+    /// @inheritdoc IFeeCollector
+    function swapBalance(bytes calldata universalRouterCalldata) external payable onlyOwner {
+        (bool success,) = universalRouter.call{value: msg.value}(universalRouterCalldata);
+        if (!success) revert InvalidUniversalRouterCalldata();
     }
 
     /// @inheritdoc IFeeCollector
--- src/interfaces/IFeeCollector.sol
+++ src/interfaces/IFeeCollector.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.13;
 
-/// @notice The collector of protocol fees.
+import {ERC20} from "solmate/tokens/ERC20.sol";
+
+/// @notice The collector of protocol fees that will be used to swap and send to a fee recipient address.
 interface IFeeCollector {
     /// @notice Swaps the token balances via universal router.
-    /// @param tokens An array of token addresses for which balances will be swapped.
-    /// @param commands Bytes data that specifies the arbitrary logic to execute in the universal router.
-    /// @param inputs Array of bytes data to serve as inputs for each command executed by the universal router.
-    /// @param deadline A timestamp indicating the latest time the operation is valid for.
-    function swapBalance(address[] calldata tokens, bytes calldata commands, bytes[] calldata inputs, uint48 deadline)
-        external;
+    /// @param universalRouterCalldata The bytes call data to be forwarded to UniversalRouter.
+    function swapBalance(bytes calldata universalRouterCalldata) external payable;
+
+    function approveAndPermit(ERC20[] calldata tokensToApprove) external;
 
     /// @notice Transfers the USDC balance from this contract to the USDC recipient.
     function withdrawFeeToken() external;
