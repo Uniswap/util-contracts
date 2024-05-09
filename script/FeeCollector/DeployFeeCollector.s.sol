c58d8c9bd47836220129211ff0a75fd8fb324f01
Eric Zhong <eric.zhong@uniswap.org>
2024-05-09 18:41:54 -0400

    add new salt

--- script/FeeCollector/DeployFeeCollector.s.sol
+++ script/FeeCollector/DeployFeeCollector.s.sol
@@ -19,7 +19,7 @@ abstract contract DeployFeeCollector is Script {
     address public constant PERMIT2 = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
 
     // For mainnet parameters, deploys the FeeCollector to 0x000000fee2Ab0fF8Dc826D3d7f45328e9Cc0471f
-    bytes32 constant SALT = bytes32(uint256(0x00000000000000000000000000000000000000008afde8517c5ada7fb0310014));
+    bytes32 constant SALT = bytes32(uint256(0x0000000000000000000000000000000000000000e6a691a183251100795200f0));
 
     function setUp() public virtual {}
 
@@ -30,7 +30,7 @@ abstract contract DeployFeeCollector is Script {
         require(params.universalRouter != address(0), "universalRouter not set");
         require(params.permit2 != address(0), "permit2 not set");
         require(params.feeToken != address(0), "feeToken not set");
-
+        
         vm.startBroadcast(privateKey);
         collector = new FeeCollector{salt: SALT}(params.owner, params.universalRouter, params.permit2, params.feeToken);
         vm.stopBroadcast();
