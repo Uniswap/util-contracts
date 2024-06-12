89178b07eacbc43cedc4b6a1c73718d9733626d0
Alice Henshaw <henshawalice@gmail.com>
2024-06-12 17:11:51 -0300

    zksync deploy script

--- script/FeeCollector/deployParameters/DeployZksync.s.sol
+++ script/FeeCollector/deployParameters/DeployZksync.s.sol
@@ -3,6 +3,17 @@ pragma solidity ^0.8.13;
 
 import {DeployFeeCollector, DeployParameters} from "../DeployFeeCollector.s.sol";
 
+/**
+    note: for now zksync-foundry does not support `forge script` command. The FeeCollector on zksync
+    was instead deployed with the following command:
+    ../foundry-zksync/target/release/forge create src/FeeCollector.sol:FeeCollector \
+    --rpc-url "https://mainnet.era.zksync.io" \
+    --zksync --chain 324 \
+    --private-key X \
+    --constructor-args 0xbE84D31B2eE049DCb1d8E7c798511632b44d1b55 0x28731BCC616B5f51dD52CF2e4dF0E78dD1136C06 0x0000000000225e31D15943971F47aD3022F714Fa 0x1d17CBcF0D6D143135aE902365D2E5e2A16538D4
+ */
+
+
 contract DeployZksync is DeployFeeCollector {
     function setUp() public override {
         params = DeployParameters({
